7a468b72a2d3e666ef5256c4957339a7ec63ae5a
Buck Doyle <b@chromatin.ca>
2019-07-01 16:20:01 -0500

    Update test to use ModestEx

--- test/modest_ex_concurrency_test.exs
+++ test/modest_ex_concurrency_test.exs
@@ -2,7 +2,8 @@ defmodule ModestExConcurrencyTest do
   use ExUnit.Case
   doctest ModestExConcurrency
 
-  test "greets the world" do
-    assert ModestExConcurrency.hello() == :world
+  test "serialises a string" do
+    assert ModestEx.serialize("<div>Hello<span>World", :body_children) ==
+             "<div>Hello<span>World</span></div>"
   end
 end
